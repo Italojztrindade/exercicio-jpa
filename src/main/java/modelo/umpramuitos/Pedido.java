57886107bafdd994bf44a75a2dccdc39b7844f78
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2023-01-18 07:20:48 -0300

    ManyToMany

--- src/main/java/modelo/umpramuitos/Pedido.java
+++ src/main/java/modelo/umpramuitos/Pedido.java
@@ -1,12 +1,15 @@
 package modelo.umpramuitos;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pedido {
@@ -16,6 +19,10 @@ public class Pedido {
 	
 	@Column(nullable = false)
 	private Date data;
+	
+	// esse é o valor default de fetch (...ToMany -> LAZY)
+	@OneToMany(mappedBy = "pedido", fetch = FetchType.LAZY)
+	private List<ItemPedido> itens;
 	
 	public Pedido() {
 		this(new Date());
@@ -43,6 +50,17 @@ public class Pedido {
 	public void setData(Date data) {
 		this.data = data;
 	}
+
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
+
+	public void setItens(List<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	
 	
 }
